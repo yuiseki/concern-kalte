e54155605aaa68f50832ec7e2965f0b08b511537
yuiseki <yuiseki@gmail.com>
2021-07-22 06:46:01 +0000

    passwordをbcryptで暗号化するところまで

--- src/models/UserModel.test.ts
+++ src/models/UserModel.test.ts
@@ -2,33 +2,62 @@ import * as mockingoose from 'mockingoose';
 import { UserModel } from './UserModel';
 
 describe('UserModel', () => {
-  it('create and save', () => {
+  it('正常にsaveできること', () => {
     const user1 = new UserModel({
       name: 'user1',
       email: 'example@example.com',
+      password: 'test',
     });
     mockingoose(UserModel).toReturn(user1, 'save');
     return expect(user1.save()).resolves.toBe(user1);
   });
   describe('validation', () => {
-    it('require email', async () => {
+    it('emailは必須', async () => {
       const user1 = new UserModel({
         name: 'user1',
+        password: 'test',
       });
       return expect(user1.save()).rejects.toThrow(
         'User validation failed: email: Path `email` is required.'
       );
     });
-    it('require unique email', async () => {
+    it('passwordは必須', async () => {
       const user1 = new UserModel({
         name: 'user1',
         email: 'example@example.com',
+      });
+      return expect(user1.save()).rejects.toThrow(
+        'User validation failed: password: Path `password` is required.'
+      );
+    });
+    it('passwordがbcryptで暗号化されていること', async () => {
+      const user1 = new UserModel({
+        name: 'user1',
+        email: 'example@example.com',
+        password: 'text',
+      });
+      mockingoose(UserModel).toReturn(null, 'save');
+      UserModel.create(user1).then((user) => {
+        // @ts-ignore
+        expect(user.password).toBe('test');
+        // @ts-ignore
+        user.save((_err, user) => {
+          expect(user.password).toMatch(/^\$2a\$10.*/);
+        });
+      });
+    });
+    it('emailはユニークでなければならない', async () => {
+      const user1 = new UserModel({
+        name: 'user1',
+        email: 'example@example.com',
+        password: 'test',
       });
       mockingoose(UserModel).toReturn(user1, 'save');
       await user1.save();
       const user2 = new UserModel({
         name: 'user2',
         email: 'example@example.com',
+        password: 'test',
       });
       mockingoose(UserModel).toReturn(1, 'countDocuments');
       return expect(user2.save()).rejects.toThrow(
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const HASH_ROUNDS = 10;
 
 interface IUser {
   name?: string;
   email: string;
+  password: string;
 }
 
 export interface IUserModel extends IUser, mongoose.Document {}
@@ -11,9 +15,24 @@ const schema = new mongoose.Schema(
   {
     name: String,
     email: { type: String, lowercase: true, required: true, unique: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
+
+schema.pre('save', async function (next) {
+  const user = this as IUserModel;
+  // passwordが変更されていなかったらなにもしない
+  if (!this.isModified('password')) next();
+  // passwordが変更されていたらbcryptで暗号化する
+  try {
+    const salt = await bcrypt.genSalt(HASH_ROUNDS);
+    user.password = await bcrypt.hash(user.password, salt);
+    return next();
+  } catch (e) {
+    return next(e);
+  }
+});
 
 schema.path('email').validate(async (value) => {
   const emailCount = await mongoose.models.User.countDocuments({
