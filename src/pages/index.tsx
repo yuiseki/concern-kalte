e730fcd70be70d4d75648c5ec4c6f44f3c644f13
yuiseki <yuiseki@gmail.com>
2021-07-22 11:17:27 +0000

    remove Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 /// <reference types="@emotion/react/types/css-prop" />
 import tw, { css } from 'twin.macro';
-import { Button } from '../components/button';
 
 const container = css`
   ${tw`mx-auto m-4 p-4 rounded bg-gray-400`}
 `;
 
-export const Page = (): JSX.Element => (
-  <div css={container}>
-    <main>
-      <h1 tw='text-5xl font-bold'>Concern Kalte</h1>
-      <Button />
-    </main>
-  </div>
-);
+export const Page = (): JSX.Element => {
+  return (
+    <div css={container}>
+      <main>
+        <h1 tw='text-5xl font-bold'>Concern Kalte</h1>
+        <a href='/api/auth/login'>Login</a>
+      </main>
+    </div>
+  );
+};
 
 export default Page;
