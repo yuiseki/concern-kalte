14e5647cd7cf9dd065dabefb7e11c763e02b9b45
yuiseki <yuiseki@gmail.com>
2021-07-23 08:50:07 +0000

    update toppage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/client';
 import { Typography } from '@material-ui/core';
 import useSWR from 'swr';
 import 'twin.macro';
+import Link from 'next/link';
 
 export const Page: React.VFC = () => {
   const [session, loading] = useSession();
@@ -13,13 +14,21 @@ export const Page: React.VFC = () => {
     <>
       {loading && <h3 tw='text-3xl font-bold'>ロード中…</h3>}
       {!loading && session && me && (
-        <h3 tw='text-3xl font-bold'>
-          {me.name ? me.name : '名無し'}({me.email}) さん、ようこそ
-        </h3>
+        <>
+          <h3 tw='text-3xl font-bold'>
+            {me.name ? me.name : '名無し'}({me.email}) さん、ようこそ
+          </h3>
+          <p>
+            <Link href='/settings'>ユーザー設定</Link>で名前を設定してください
+          </p>
+        </>
       )}
       {!loading && !session && (
         <Typography variant='h6'>
-          ログインまたはユーザー登録してください
+          <Link href='/auth/signup'>ユーザー登録</Link>
+          または
+          <Link href='/auth/signin'>ログイン</Link>
+          してください
         </Typography>
       )}
     </>
