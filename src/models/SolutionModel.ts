1fe244592db610b7d7041205e72ecc2346f3f104
yuiseki <yuiseki@gmail.com>
2021-07-24 16:03:17 +0900

    fix TeamModel, add SolutionModel

---
+++ src/models/SolutionModel.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+interface ISolution {
+  name: string;
+}
+
+export interface ISolutionModel extends ISolution, mongoose.Document {}
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const SolutionModel =
+  mongoose.models.SolutionModel || mongoose.model<ISolutionModel>('SolutionModel', schema);
