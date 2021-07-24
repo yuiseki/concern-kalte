e460688d06c176d4a21e094c5d3a9edd4cb3ffb0
yuiseki <yuiseki@gmail.com>
2021-07-24 16:55:58 +0900

    add TeamModel.solutions

--- src/models/TeamModel.ts
+++ src/models/TeamModel.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+import { ISolutionModel } from './SolutionModel';
+import { IUserModel } from './UserModel';
 
 interface ITeam {
   name: string;
-  users?: []
+  users?: IUserModel[];
+  solutions?: ISolutionModel[];
 }
 
 export interface ITeamModel extends ITeam, mongoose.Document {}
@@ -10,7 +13,8 @@ export interface ITeamModel extends ITeam, mongoose.Document {}
 const schema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    users: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'UserModel' }]
+    users: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'UserModel' }],
+    solutions: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'SolutionModel' }],
   },
   { timestamps: true }
 );
