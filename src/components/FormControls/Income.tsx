fdf99b783ab20e0cfed4e125ff9be6483bc01dcb
yuiseki <yuiseki@gmail.com>
2021-07-30 08:13:45 +0900

    世帯年収スライダーを追加

--- src/components/FormControls/Income.tsx
+++ src/components/FormControls/Income.tsx
@@ -43,3 +43,44 @@ export const PersonalYearlyIncomeFormControl: React.VFC = () => {
     </FormControl>
   );
 };
+
+export const HouseholdYearlyIncomeFormControl: React.VFC = () => {
+  const name = 'household-yearly-income';
+  const [value, setValue] = useLocalStorageValue(name, '0');
+
+  // @ts-ignore
+  const [defaultValue, setDefaultValue] = useState<string>(value);
+
+  const valueLabelFormat = (value) => {
+    if (value === null) {
+      value = 0;
+    }
+    return `${value} 万円`;
+  };
+
+  useEffect(() => {
+    // @ts-ignore
+    setDefaultValue(parseInt(value));
+  }, [value]);
+
+  return (
+    <FormControl variant='outlined'>
+      <Typography id='input-slider' gutterBottom>
+        世帯年収：{valueLabelFormat(value)}
+      </Typography>
+      <Box sx={{ width: 200 }}>
+        <Slider
+          step={50}
+          marks
+          min={0}
+          max={1000}
+          value={parseInt(defaultValue)}
+          onChange={(e, val) => {
+            // @ts-ignore
+            setValue(val);
+          }}
+        />
+      </Box>
+    </FormControl>
+  );
+};
