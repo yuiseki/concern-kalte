fff10b7a2a92996298b7e5df0282bf113459fcbc
yuiseki <yuiseki@gmail.com>
2021-07-31 13:39:25 +0900

    impl /recipes/[id]

---
+++ src/pages/recipes/[id].tsx
@@ -0,0 +1,11 @@
+import { Layout } from '~/components/Layout';
+
+const Page: React.VFC = () => {
+  return (
+    <Layout>
+      <h1 tw='text-2xl'>お悩み解決レシピ</h1>
+    </Layout>
+  );
+};
+
+export default Page;
